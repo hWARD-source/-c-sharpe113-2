b53a46647fe36872d7ce83bf752dd37b9f2ba2bf
hWARD-source <a113221026@mail.shu.edu.tw>
2025-03-20 12:57:03 +0800

    114-3-20

---
+++ 113_2_C_Sharp_Tutorial-main/114_03_13/Program6_10/Program6_10/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program6_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand = new Random();
+        string compChoice, myChoice, winner;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCompChoice();
+        }
+
+        private void getCompChoice()
+        {
+            int n = rand.Next(1, 4);
+            switch (n)
+            {
+                case 1:
+                    compChoice == "石頭";
+                    break;
+                case 2:
+                    compChoice = "布";
+                    break;
+                case 3:
+                    compChoice = :"剪刀";
+                    break;
+            }
+        }
+
+        private void showWinner()
+        {
+            string winner "";
+
+            //winner = winnerDecide(myChoice); //Call byvalue 
+            winnerDecide(myChoice,  ref winner);      //Call by reference 
+
+            label.Text = "電腦出: " + compChoice + "玩家出" + myChoice + "/n" + winner;
+
+            if (winner = "電腦贏!")
+                comScore++;
+            else if (winner = "玩家贏!")
+                playScore++;
+            else
+                tieScore++;
+        }
+
+        //private string winnerDecide(string myChoice)
+       // {
+            //string winnerwho;
+            //if (myChoice == comChoice)
+                //winner = "平手";
+            //else if (myChoice == "石頭" && compChoice == "剪刀")
+                //winner = "玩家贏";
+            //else if (myChoice == "布" && compChoice == "石頭")
+                //winner = "玩家贏";
+            //else if (myChoice == "剪刀" && compChoice == "布")
+                //winner = "玩家贏";
+            //else
+                //winner = "電腦贏!";
+            //return winnerwho;
+            //}
+
+        private void winnerDecide(string myChoice ,ref string winner)    //Call by reference
+        {
+            if (myChoice == comChoice)
+               /winner = "平手";
+            else if (myChoice == "石頭" && compChoice == "剪刀")
+                winner = "玩家贏";
+            else if (myChoice == "布" && compChoice == "石頭")
+                winner = "玩家贏";
+            else if (myChoice == "剪刀" && compChoice == "布")
+                winner = "玩家贏";
+                else
+                winner = "電腦贏!";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myChoice = "Rock";
+            showWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myChoice = "Paper";
+            showWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myChoice = "Scissors";
+            showWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getCompChoice();
+            label1.Text = ";"
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Message.Show("電腦贏了" + compChoice + "次\n " + "玩家贏" + playScore + "次")
+            this.Close();
+           
+        }
+    }
+}
---
+++ 114-3-20/Tutorial 7-1/Lottery Numbers/Lottery Numbers/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lottery_Numbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            const int SIZE = 5;  //The size of the Array
+            int[] lotteryNumbers = new int[SIZE]; //儲存樂透號碼陣列
+            Random rand = new Random();
+
+            for (int i = 0; i < lotteryNumbers.Length; i++)
+            {
+                lotteryNumbers[i] = rand.Next(1, 43); //產生1到42的亂數
+            }
+
+            // firstLabel1.Text = lotteryNumbers[0].ToString();
+            //secondLabel1.Text = lotteryNumbers[1].ToString();
+            //thirdLabel1.Text = lotteryNumbers[2].ToString();
+            //fourthLabel1.Text = lotteryNumbers[3].ToString();
+            //fifthLabel1.Text = lotteryNumbers[4].ToString();
+            //使用迴圈顯示樂透號碼
+            Label[] showLLabels = { firstLabel, secondLabel, thirdLabel, fourthLabel, fifthLabel] };
+            for (int i = 0; i < showLabels.Length; i++)
+            {
+                showLLabels[i].Text = lotteryNumbers[i].ToString();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
